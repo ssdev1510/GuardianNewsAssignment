1c0e83b033aae097f752d8ef68255463edd6aeee
Sahil Singla <sahil.singla@iceye.fi>
2023-07-07 18:45:54 +0200

    added logic to similarity score of two different string

--- src/main/java/utils/TestUtils.java
+++ src/main/java/utils/TestUtils.java
@@ -21,6 +21,40 @@ public class TestUtils {
 		return prop.getProperty(key);
 	}
 
-	
+	// Check the score of two strings how much it matches. If the percentage is more
+	// than 30% than it is return true otherwise false
+	// @Return : Boolean > True in case of two string matching score is greater than
+	// 30 other wise false
+	public static boolean checkStringsSimilarityScore(String guardianNews, String otherSourceNews) {
+		boolean result = false;
+		int count = 0;
+		guardianNews = guardianNews.toLowerCase();
+		otherSourceNews = otherSourceNews.toLowerCase();
+
+		String[] otherSourceNewsWords = otherSourceNews.split("[\\s':-]+");
+		for (String str : otherSourceNewsWords) {
+			if (guardianNews.contains(str)) {
+				count++;
+				double score = (double) count / otherSourceNewsWords.length;
+				Log.debug(score);
+				if (score > 0.50) {
+					result = true;
+					break;
+				} else {
+					continue;
+				}
+			}
+		}
+		return result;
+	}
+
+	// Get number of similar articles from other sources
+	// @Return : integer > number of articles
+	public static boolean checkOtherResoucesCountGreaterOrEqualTwo(int googleCount, int BBCCount, int expectedCouunt) {
+		return googleCount >= expectedCouunt || BBCCount >= expectedCouunt;
+
+	}
+
+	// Screenshots
 
 }
